9178eaf96990456eefe2d49298c9eb81cf309cc1
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-08-07 20:03:11 +0600

    order amount issue fix

--- config/app.php
+++ config/app.php
@@ -232,7 +232,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'Cart' => Gloudemans\Shoppingcart\Facades\Cart::class,
     ],
