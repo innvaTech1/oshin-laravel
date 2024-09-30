44e39409b17b45ecf06cf6abaedf57bedee71f27
progRizvi <prog.rizvi@gmail.com>
2024-09-30 22:40:30 +0600

    state and city issue fix

--- app/Http/Controllers/Admin/CityController.php
+++ app/Http/Controllers/Admin/CityController.php
@@ -11,6 +11,7 @@ use Str;
 use App\Models\BillingAddress;
 use App\Models\ShippingAddress;
 use App\Models\User;
+
 class CityController extends Controller
 {
     public function __construct()
@@ -20,11 +21,11 @@ class CityController extends Controller
 
     public function index()
     {
-        $cities=City::all();
+        $cities = City::orderBy('name', 'asc')->get();
         $billingAddress = BillingAddress::all();
         $shippingAddress = ShippingAddress::all();
         $users = User::all();
-        return view('admin.city', compact('cities','billingAddress','shippingAddress','users'));
+        return view('admin.city', compact('cities', 'billingAddress', 'shippingAddress', 'users'));
     }
 
 
@@ -38,27 +39,27 @@ class CityController extends Controller
     public function store(Request $request)
     {
         $rules = [
-            'state'=>'required',
-            'name'=>'required|unique:cities'
+            'state' => 'required',
+            'name' => 'required'
         ];
 
         $customMessages = [
-            
+
             'state.required' => trans('admin_validation.State is required'),
             'name.required' => trans('admin_validation.Name is required'),
             'name.unique' => trans('admin_validation.Name already exist'),
         ];
-        $this->validate($request, $rules,$customMessages);
+        $this->validate($request, $rules, $customMessages);
 
-        $city=new City();
-        $city->country_state_id=$request->state;
-        $city->name=$request->name;
-        $city->slug=Str::slug($request->name);
-        $city->status=$request->status;
+        $city = new City();
+        $city->country_state_id = $request->state;
+        $city->name = $request->name;
+        $city->slug = Str::slug($request->name);
+        $city->status = $request->status;
         $city->save();
 
-        $notification=trans('admin_validation.Created Successfully');
-        $notification=array('messege'=>$notification,'alert-type'=>'success');
+        $notification = trans('admin_validation.Created Successfully');
+        $notification = array('messege' => $notification, 'alert-type' => 'success');
         return redirect()->back()->with($notification);
     }
 
@@ -67,7 +68,7 @@ class CityController extends Controller
     {
         $states = CountryState::all();
         $city = City::find($id);
-        return view('admin.edit_city', compact('states','city'));
+        return view('admin.edit_city', compact('states', 'city'));
     }
 
 
@@ -75,24 +76,24 @@ class CityController extends Controller
     {
         $city = City::find($id);
         $rules = [
-            'state'=>'required',
-            'name'=>'required|unique:cities,name,'.$city->id
+            'state' => 'required',
+            'name' => 'required'
         ];
         $customMessages = [
-            
+
             'state.required' => trans('admin_validation.State is required'),
             'name.required' => trans('admin_validation.Name is required'),
             'name.unique' => trans('admin_validation.Name already exist'),
         ];
-        $this->validate($request, $rules,$customMessages);
-        $city->country_state_id=$request->state;
-        $city->name=$request->name;
-        $city->slug=Str::slug($request->name);
-        $city->status=$request->status;
+        $this->validate($request, $rules, $customMessages);
+        $city->country_state_id = $request->state;
+        $city->name = $request->name;
+        $city->slug = Str::slug($request->name);
+        $city->status = $request->status;
         $city->save();
 
-        $notification=trans('admin_validation.Update Successfully');
-        $notification=array('messege'=>$notification,'alert-type'=>'success');
+        $notification = trans('admin_validation.Update Successfully');
+        $notification = array('messege' => $notification, 'alert-type' => 'success');
         return redirect()->route('admin.city.index')->with($notification);
     }
 
@@ -101,21 +102,22 @@ class CityController extends Controller
     {
         $city = City::find($id);
         $city->delete();
-        $notification=trans('admin_validation.Delete Successfully');
-        $notification=array('messege'=>$notification,'alert-type'=>'success');
+        $notification = trans('admin_validation.Delete Successfully');
+        $notification = array('messege' => $notification, 'alert-type' => 'success');
         return redirect()->route('admin.city.index')->with($notification);
     }
 
-    public function changeStatus($id){
+    public function changeStatus($id)
+    {
         $city = City::find($id);
-        if($city->status==1){
-            $city->status=0;
+        if ($city->status == 1) {
+            $city->status = 0;
             $city->save();
-            $message=trans('admin_validation.Inactive Successfully');
-        }else{
-            $city->status=1;
+            $message = trans('admin_validation.Inactive Successfully');
+        } else {
+            $city->status = 1;
             $city->save();
-            $message=trans('admin_validation.Active Successfully');
+            $message = trans('admin_validation.Active Successfully');
         }
         return response()->json($message);
     }
--- app/Http/Controllers/Admin/CountryStateController.php
+++ app/Http/Controllers/Admin/CountryStateController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\CountryState;
-use Str;
+use App\Models\User;
 use App\Models\Country;
+use Illuminate\Support\Str;
+
+use App\Models\CountryState;
+use Illuminate\Http\Request;
 use App\Models\BillingAddress;
 use App\Models\ShippingAddress;
-use App\Models\User;
+use App\Http\Controllers\Controller;
+
 class CountryStateController extends Controller
 {
     public function __construct()
@@ -19,17 +21,17 @@ class CountryStateController extends Controller
 
     public function index()
     {
-        $states=CountryState::with('cities')->get();
+        $states = CountryState::with('cities')->orderBy('name', 'asc')->get();
         $billingAddress = BillingAddress::all();
         $shippingAddress = ShippingAddress::all();
         $users = User::all();
-        return view('admin.state', compact('states','billingAddress','shippingAddress','users'));
+        return view('admin.state', compact('states', 'billingAddress', 'shippingAddress', 'users'));
     }
 
 
     public function create()
     {
-        
+
         return view('admin.create_state');
     }
 
@@ -37,22 +39,22 @@ class CountryStateController extends Controller
     public function store(Request $request)
     {
         $rules = [
-            'name'=>'required|unique:country_states'
+            'name' => 'required'
         ];
         $customMessages = [
             'name.required' => trans('admin_validation.Name is required'),
             'name.unique' => trans('admin_validation.Name already exist'),
         ];
-        $this->validate($request, $rules,$customMessages);
+        $this->validate($request, $rules, $customMessages);
 
-        $state=new CountryState();
-        $state->name=$request->name;
-        $state->slug=Str::slug($request->name);
-        $state->status=$request->status;
+        $state = new CountryState();
+        $state->name = $request->name;
+        $state->slug = Str::slug($request->name);
+        $state->status = $request->status;
         $state->save();
 
-        $notification=trans('admin_validation.Created Successfully');
-        $notification=array('messege'=>$notification,'alert-type'=>'success');
+        $notification = trans('admin_validation.Created Successfully');
+        $notification = array('messege' => $notification, 'alert-type' => 'success');
         return redirect()->back()->with($notification);
     }
 
@@ -60,8 +62,8 @@ class CountryStateController extends Controller
     public function edit($id)
     {
         $state = CountryState::find($id);
-        $countries=Country::all();
-        return view('admin.edit_state', compact('state','countries'));
+        $countries = Country::all();
+        return view('admin.edit_state', compact('state', 'countries'));
     }
 
 
@@ -69,7 +71,7 @@ class CountryStateController extends Controller
     {
         $state = CountryState::find($id);
         $rules = [
-            'name'=>'required|unique:country_states,name,'.$state->id,
+            'name' => 'required',
             'status' => 'required'
         ];
         $customMessages = [
@@ -77,15 +79,15 @@ class CountryStateController extends Controller
             'name.required' => trans('admin_validation.Name is required'),
             'name.unique' => trans('admin_validation.Name already exist'),
         ];
-        $this->validate($request, $rules,$customMessages);
+        $this->validate($request, $rules, $customMessages);
 
-        $state->name=$request->name;
-        $state->slug=Str::slug($request->name);
-        $state->status=$request->status;
+        $state->name = $request->name;
+        $state->slug = Str::slug($request->name);
+        $state->status = $request->status;
         $state->save();
 
-        $notification=trans('admin_validation.Updated Successfully');
-        $notification=array('messege'=>$notification,'alert-type'=>'success');
+        $notification = trans('admin_validation.Updated Successfully');
+        $notification = array('messege' => $notification, 'alert-type' => 'success');
         return redirect()->route('admin.state.index')->with($notification);
     }
 
@@ -94,21 +96,22 @@ class CountryStateController extends Controller
     {
         $state = CountryState::find($id);
         $state->delete();
-        $notification=trans('admin_validation.Delete Successfully');
-        $notification=array('messege'=>$notification,'alert-type'=>'success');
+        $notification = trans('admin_validation.Delete Successfully');
+        $notification = array('messege' => $notification, 'alert-type' => 'success');
         return redirect()->route('admin.state.index')->with($notification);
     }
 
-    public function changeStatus($id){
+    public function changeStatus($id)
+    {
         $state = CountryState::find($id);
-        if($state->status==1){
-            $state->status=0;
+        if ($state->status == 1) {
+            $state->status = 0;
             $state->save();
-            $message= trans('admin_validation.Inactive Successfully');
-        }else{
-            $state->status=1;
+            $message = trans('admin_validation.Inactive Successfully');
+        } else {
+            $state->status = 1;
             $state->save();
-            $message= trans('admin_validation.Active Successfully');
+            $message = trans('admin_validation.Active Successfully');
         }
         return response()->json($message);
     }
