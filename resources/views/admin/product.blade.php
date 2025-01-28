32fa2808fc5188b10a180edb9be4a04c2b131609
Riajul Islam <ritewu2014@gmail.com>
2025-01-28 18:49:10 +0600

    settings button restored

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -84,11 +84,13 @@
                                                         <a href="{{ route('admin.product.edit', $product->id) }}"
                                                             class="btn btn-primary btn-sm"><i class="fa fa-edit"
                                                                 aria-hidden="true"></i></a>
+
                                                         @php
                                                             $existOrder = $orderProducts
                                                                 ->where('product_id', $product->id)
                                                                 ->count();
                                                         @endphp
+
                                                         @if ($existOrder == 0)
                                                             <a href="javascript:;" data-toggle="modal"
                                                                 data-target="#deleteModal" class="btn btn-danger btn-sm"
@@ -100,6 +102,35 @@
                                                                 class="btn btn-danger btn-sm" disabled><i
                                                                     class="fa fa-trash" aria-hidden="true"></i></a>
                                                         @endif
+
+
+                                                        <div class="dropdown d-inline">
+                                                            <button class="btn btn-primary btn-sm dropdown-toggle"
+                                                                type="button" id="dropdownMenuButton2"
+                                                                data-toggle="dropdown" aria-haspopup="true"
+                                                                aria-expanded="false">
+                                                                <i class="fas fa-cog"></i>
+                                                            </button>
+
+                                                            <div class="dropdown-menu" x-placement="top-start"
+                                                                style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, -131px, 0px);">
+                                                                <a class="dropdown-item has-icon"
+                                                                    href="{{ route('admin.product-gallery', $product->id) }}"><i
+                                                                        class="far fa-image"></i>
+                                                                    {{ __('Image Gallery') }}</a>
+
+                                                                <a class="dropdown-item has-icon"
+                                                                    href="{{ route('admin.product-highlight', $product->id) }}"><i
+                                                                        class="fas fa-lightbulb"></i>
+                                                                    {{ __('Highlight') }}</a>
+
+                                                                <a class="dropdown-item has-icon"
+                                                                    href="{{ route('admin.product-variant', $product->id) }}"><i
+                                                                        class="fas fa-cog"></i>{{ __('Product Variant') }}</a>
+
+                                                            </div>
+                                                        </div>
+
                                                     </td>
                                                 </tr>
                                             @endforeach
