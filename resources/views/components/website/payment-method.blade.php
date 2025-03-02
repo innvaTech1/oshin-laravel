caee9e0941921128a3d7b2ea097cf69bdb850cba
Riajul Islam <ritewu2014@gmail.com>
2025-03-02 23:47:30 +0600

    button updated

--- resources/views/components/website/payment-method.blade.php
+++ resources/views/components/website/payment-method.blade.php
@@ -14,19 +14,17 @@
                     </span>
                 </label>
             @endif
-
             @if ($aamarpay && $aamarpay->status)
                 <label
                     class="payment-item bg-light w-100 p-3 d-flex align-items-center gap-3 cursor-pointer rounded border border-secondary">
-                    <input type="radio" name="payment_method" value="aamarpay" class="payment-radio d-none"
-                        onclick="setPaymentMethod('aamarpay')" data-method="aamarpay"
-                        data-store-id="{{ 'aamarpay' }}"
-                        data-signature-key="{{ '28c78bb1f45112f5d40b956fe104645a' }}"
-                        data-currency="{{ $aamarpay->currency }}" data-country="{{ $aamarpay->country }}">
+                    <input type="radio" name="payment_method" value="Aamarpay" class="payment-radio d-none"
+                        onclick="setPaymentMethod('aamarpay')">
                     <div class="custom-radio-icon d-flex justify-content-center align-items-center">
                         <span class="checkmark bg-white border border-secondary rounded-circle"></span>
                     </div>
-                    <span class="text-dark font-weight-bold text-base">Aamarpay</span>
+                    <span class="text-dark font-weight-bold text-base">
+                        {{ 'Aamarpay' }}
+                    </span>
                 </label>
             @endif
         </div>
