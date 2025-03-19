f79eeab3068f948aa4589c4acad0b28a50994814
Riajul Islam <ritewu2014@gmail.com>
2025-03-20 04:19:22 +0600

    Help & Support route changed

--- resources/views/website/partials/top-bar.blade.php
+++ resources/views/website/partials/top-bar.blade.php
@@ -10,7 +10,7 @@
                         <a href="{{ route('track-order') }}">Track Your Order</a>
                     </li>
                     <li>
-                        <a href="{{ route('help-support') }}">Help & Support</a>
+                        <a href="{{ route('contact-us') }}">Help & Support</a>
                     </li>
                 </ul>
             </div>
