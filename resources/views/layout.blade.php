3773420da92d0442f63572d294e486cc4d1e0097
Riajul Islam <ritewu2014@gmail.com>
2025-04-19 02:59:25 +0600

    removed tawk.to

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -570,23 +570,6 @@
                 </div>
             </div>
         </div>
-
-        <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-            var Tawk_API = Tawk_API || {},
-                Tawk_LoadStart = new Date();
-            (function() {
-                var s1 = document.createElement("script"),
-                    s0 = document.getElementsByTagName("script")[0];
-                s1.async = true;
-                s1.src = 'https://embed.tawk.to/67ac68213a842732607dbeeb/1ijsndh9m';
-                s1.charset = 'UTF-8';
-                s1.setAttribute('crossorigin', '*');
-                s0.parentNode.insertBefore(s1, s0);
-            })();
-        </script>
-        <!--End of Tawk.to Script-->
-
     </footer>
 
     <!--============================
