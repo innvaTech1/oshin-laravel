237a2a38658832c18f23bcd6b8ef34166fbd581d
Riajul Islam <ritewu2014@gmail.com>
2025-05-16 19:11:29 +0600

    sidebar buttons fixed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -142,16 +142,38 @@
             z-index: 9999;
             transition: opacity 0.3s ease-out;
         }
-        
+
         #page-loader img {
             max-width: 150px;
             animation: pulse 1.5s infinite;
         }
-        
+
         @keyframes pulse {
-            0% { transform: scale(1); }
-            50% { transform: scale(1.1); }
-            100% { transform: scale(1); }
+            0% {
+                transform: scale(1);
+            }
+
+            50% {
+                transform: scale(1.1);
+            }
+
+            100% {
+                transform: scale(1);
+            }
+        }
+
+        .custom-button {
+            padding: 10px 20px;
+            border: #930A02 solid 1px;
+            border-radius: 5px;
+            background-color: white;
+            color: #930a02;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .custom-button:hover {
+            background-color: #930a02;
+            color: white;
         }
     </style>
 </head>
--- resources/views/website/partials/header-2.blade.php
+++ resources/views/website/partials/header-2.blade.php
@@ -110,9 +110,8 @@
                         <span>{{ currency_icon() }}{{ $sidebarCartSubTotal }}</span>
                     </h5>
                     <div class="wsus__minicart_btn_area">
-                        <a class="btn btn-lg btn-danger" href="{{ route('cart') }}">{{ __('user.View Cart') }}</a>
-                        <a class="btn btn-lg btn-danger"
-                            href="{{ route('checkout.checkout') }}">{{ __('user.Checkout') }}</a>
+                        <a class="custom-button" href="{{ route('cart') }}">{{ __('user.View Cart') }}</a>
+                        <a class="custom-button" href="{{ route('checkout.checkout') }}">{{ __('user.Checkout') }}</a>
                     </div>
                 @endif
             </div>
